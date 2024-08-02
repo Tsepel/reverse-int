5da0d9705141b7b614d45e004ae4d5b7de35407a
Tsepel <tsepelevaa@gmail.com>
2024-08-02 18:56:59 +0300

    feat: update function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 module.exports = function reverse (n) {
-    const str = n.toString();
+    let str = n.toString();
     let reversedStr = '';
+
+    if (n < 0) {
+        str = str.slice(1, str.length);
+    }
 
     for (let i = str.length - 1; i >= 0; i--) {
         reversedStr += str[i];
-    }
-
-    if (n < 0) {
-        reversedStr = reversedStr.slice(0, reversedStr.length - 1);
     }
 
     return reversedStr;
